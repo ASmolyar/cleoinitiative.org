2443a568235a0427663e4dd0d0a9a17cf4d4d573
Aaron Smolyar <57324530+ASmolyar@users.noreply.github.com>
2024-11-09 18:17:46 -0500

    again

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
   base: '/',
   build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        manifest: resolve(__dirname, 'public/site.webmanifest'),
-      },
-    },
+    outDir: 'dist',
+    assetsDir: 'assets',
+    copyPublicDir: true,
   },
 }) 
